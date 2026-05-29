191569b109dfac90e7e07eb70618397affff95e3
DistrictBlauw <districtblauw@users.noreply.github.com>
2026-05-29 23:57:43 +0800

    fix: add missing OutlinedButton import in Credits.kt

--- app/src/main/java/redhead/app/simpleboot/Credits.kt
+++ app/src/main/java/redhead/app/simpleboot/Credits.kt
@@ -17,9 +17,8 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
